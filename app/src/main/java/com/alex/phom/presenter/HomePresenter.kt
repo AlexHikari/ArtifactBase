b10f5ef4a365e000aae684d37643abe5202e9d03
AlexHikari <alriv01@ucm.es>
2018-11-27 19:27:55 +0100

    New gallery icon added

--- app/src/main/java/com/alex/phom/presenter/HomePresenter.kt
+++ app/src/main/java/com/alex/phom/presenter/HomePresenter.kt
@@ -9,10 +9,10 @@ class HomePresenter(view: HomePresenter.View, errorHandler: ErrorHandler) :
 
     override fun initialize() {
         val listItems: List<AHBottomNavigationItem> = listOf(
-                AHBottomNavigationItem(R.string.tab_1, R.drawable.news512, R.color.colorBlueSource),
-                AHBottomNavigationItem(R.string.tab_2, R.drawable.news512, R.color.colorRedSource),
-                AHBottomNavigationItem(R.string.tab_3, R.drawable.news512, R.color.colorGreenSource),
-                AHBottomNavigationItem(R.string.tab_4, R.drawable.news512, R.color.colorGreenSource))
+                AHBottomNavigationItem(R.string.tab_1, R.drawable.newsicon, R.color.colorBlueSource),
+                AHBottomNavigationItem(R.string.tab_2, R.drawable.galleryicon, R.color.colorRedSource),
+                AHBottomNavigationItem(R.string.tab_3, R.drawable.galleryicon, R.color.colorGreenSource),
+                AHBottomNavigationItem(R.string.tab_4, R.drawable.galleryicon, R.color.colorGreenSource))
         view.initializeBottomNavigationView(listItems)
         view.showNewsScreen()
     }
--- app/src/main/java/com/alex/phom/view/adapter/CardAdapter.kt
+++ app/src/main/java/com/alex/phom/view/adapter/CardAdapter.kt
@@ -13,28 +13,6 @@ class CardAdapter(onItemClickListener: (Cardview) -> Unit) : RootAdapter<Cardvie
 
     class ViewHolder(view: View) : RootViewHolder<Cardview>(itemView = view) {
         override fun bind(model: Cardview) {
-
-            
-            if (model.attack != null && model.attack > 0) {
-                itemView.cardAttack.text = model.attack.toString()
-            } else {
-                itemView.cardAttack.visibility = View.INVISIBLE
-            }
-            if (model.armor != null && model.armor > 0) {
-                itemView.cardArmor.text = model.armor.toString()
-            } else {
-                itemView.cardArmor.visibility = View.INVISIBLE
-            }
-            if (model.hitPoints != null && model.hitPoints > 0) {
-                itemView.cardHitPoints.text = model.hitPoints.toString()
-            } else {
-                itemView.cardHitPoints.visibility = View.INVISIBLE
-            }
-            if (model.cardText.english != null) {
-                itemView.cardText.text = model.cardText.english
-            } else {
-                itemView.cardText.text = "No text provided"
-            }
             if (model.largeImage.default != null) {
                 itemView.cardImage.load(model.largeImage.default)
             }
